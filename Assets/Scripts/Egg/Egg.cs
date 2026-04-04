aee6742fae1cd3150d8c43afd2595e95add380e9
MasterSTime126 <hello.neinbor@gmail.com>
2026-04-04 18:06:35 +0500

    Egg prefab add RB2D, UI scene add

--- Assets/Scripts/Egg/Egg.cs
+++ Assets/Scripts/Egg/Egg.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
-
 public class Egg : Entity
 {
     public float HatchProgress;
     public float HatchTime;
     public float HatchSpeed;
+
+
+    // CHECKPOINT: Here I just removed the ability to move, attack, and rotate for the egg
+    protected override void MoveTowardsTarget()
+    {
+        // Eggs shouldnt move
+    }
+
+    protected override void HandleAnimation()
+    {
+        // Eggs shouldnt rotate?
+    }
+
+    protected override void Attack()
+    {
+        // Eggs shouldnt attack
+    }
 }
